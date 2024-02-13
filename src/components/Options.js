91e4b00628465747515078cd654bd6443e4595dd
Moaaz Ayman <moaazayman71@gmail.com>
2024-02-13 11:44:28 +0200

    first commit

---
+++ src/components/Options.js
@@ -0,0 +1,25 @@
+function Options({ question, dispatch, answer }) {
+  const answered = answer !== null;
+  return (
+    <div className="options">
+      {question.options.map((option, index) => (
+        <button
+          className={`btn btn-option ${index === answer ? "answer" : ""} ${
+            answered
+              ? index === question.correctOption
+                ? "correct"
+                : "wrong"
+              : ""
+          }`}
+          key={index}
+          disabled={answered}
+          onClick={() => dispatch({ type: "newAnswer", payload: index })}
+        >
+          {option}
+        </button>
+      ))}
+    </div>
+  );
+}
+
+export default Options;
